c6a8fea5b188830f47ce0d80365c4431aa3b373f
fsy <fsy@outlook.com>
2022-06-14 11:42:05 +0800

    Update copythat.meta.js

--- copythat.meta.js
+++ copythat.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         D Clippy
 // @namespace    https://github.com/vonsy
-// @version      0.0.2
+// @version      0.0.3
 // @description  D板大眼夹,一些小功能.
 // @author       fsy <fsy@outlook.com>
 // @license      MIT License
-// @match        https://www.hi-pda.com/forum/viewthread.php*
+// @match        https://www.4d4y.com/forum/viewthread.php*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/vonsy/copythat/main/copythat.meta.js
 // @downloadURL  https://raw.githubusercontent.com/vonsy/copythat/main/copythat.user.js
